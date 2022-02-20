0aab1780618fbce828f64f32ab8701f98d896dd0
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-02-20 11:28:22 +0100

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { actions } from 'react-redux-form';
 import Menu  from "./MenuComponent"
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
@@ -20,7 +21,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 
 });
 
@@ -58,7 +60,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/>}/>
           <Route path="/menu/:dishId" component={DishWithId}/>
-          <Route exact path="/contactus" component={Contact}/>
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm}/>}/>
           <Route exact path="/aboutus" component={() =><About leaders={this.props.leaders}/>} />
           <Redirect to="/home" />
         </Switch>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
 import { Comments } from "./comments";
 import { Dishes } from "./dishes";
 import { Leaders } from "./leaders";
 import { Promotions } from "./promotions";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import {InitialFeedback} from './forms';
 
 export const ConfigureStore = () => {
     const Store = createStore(
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
             leaders: Leaders,
             comments: Comments,
             dishes: Dishes,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         // The applyMiddleware returns store enhancers that will be supplied to the application.
         applyMiddleware(thunk, logger)
